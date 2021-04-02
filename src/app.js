e25523994353ae7a8fa9e86f8eb081b75975bf34
talya19 <talya.grausz@gmail.com>
2021-04-02 15:47:52 +0200

    showing current temperature and wind and humidity

--- src/app.js
+++ src/app.js
@@ -1,10 +1,18 @@
 function displayTemperature (response){
-    console.log(response.data.main.temp);
+    console.log(response.data);
     let temperatureElement=document.querySelector("#temperature");
     temperatureElement.innerHTML=Math.round(response.data.main.temp);
+    let cityElement=document.querySelector("#city");
+    cityElement.innerHTML=response.data.name;
+    let descriptionElement=document.querySelector("#description");
+    descriptionElement.innerHTML=response.data.weather[0].description;
+    let humidityElement=document.querySelector("#humidity");
+    humidityElement.innerHTML=response.data.main.humidity;
+    let windElement=document.querySelector("#wind");
+    windElement.innerHTML=Math.round(response.data.wind.speed);
 }
 
 let apiKey="e0f9cf2497fe0ed03d06e212d42c8fba";
 let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
 
-axios.get(apiUrl).then (displayTemperature);
+axios.get(apiUrl).then(displayTemperature);
